d8730b72c91a1b946a73bcfdab38ef4c499c7a11
pceinstein <pceinstein@hotmail.com>
2023-02-26 20:21:27 -0300

    Views para alteração de senha

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+
+    # urls para alteração de senha
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
