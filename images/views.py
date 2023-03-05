e33ecbfdd7373ebf5381652c125fc7b29a1cbda8
pceinstein <pceinstein@hotmail.com>
2023-03-05 13:35:57 -0300

    Postando conteúdo de outros sites

--- images/views.py
+++ images/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreatForm
 
 # Create your views here.
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # formulário foi enviado
+        form = ImageCreatForm(data=request.POST)
+        if form.is_valid():
+            # os dados do formulário são válidos
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+
+            # atribui o usuário atual ao item
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+
+            # redireciona para a view de detalhes do novo item criado
+            return redirect(new_item.get_absolute_url())
+    
+    else:   # request.GET
+        # cria o formulário com os dados fornecidos
+        # pelo bookmarklet via GET
+        form = ImageCreatForm(request.GET)
+
+    return render(request,
+                  'images/image/create.html',
+                  {'section': 'images',
+                   'form': form})
