560f8ff6556d8c063a9600127a182dccf68984b4
pceinstein <pceinstein@hotmail.com>
2023-06-23 11:41:15 -0300

    twitter auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -160,7 +160,7 @@ SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['SOCIAL_Facebook_Secret']  # Senha do A
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']  # Permissões extras para solicitar aos usuários do Facebook
 
 SOCIAL_AUTH_TWITTER_KEY = os.environ['SOCIAL_Twitter_Key']          # ID do App Bookmarks no Twitter
-SOCIAL_AUTH_TWITER_SECRET = os.environ['SOCIAL_Twitter_Secret']     # Senha do App Bookmarks no Twitter
+SOCIAL_AUTH_TWITTER_SECRET = os.environ['SOCIAL_Twitter_Secret']    # Senha do App Bookmarks no Twitter
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_Google_Key']     # ID do App Bookmarks no Google
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['SOCIAL_Google_Key']         # ID do App Bookmarks no Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['SOCIAL_Google_Secret']   # Senha do App Bookmarks no Google
